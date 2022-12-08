15be059e74ed5c3621ac64f91787f785648b7ddd
Melonisjy <dhy2457@naver.com>
2022-12-09 00:56:33 +0900

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const navigate = useNavigate();
+
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+
+  const navigateToSignup = () => {
+    navigate("/signup");
+  };
+  return (
+    <header className="header">
+      <strong>Fithub</strong>
+      <ul>
+        <li>
+          <Button variant="outlined" size="medium" onClick={navigateToLogin}>
+            로그인
+          </Button>
+        </li>
+        <li>
+          <Button variant="outlined" size="medium" onClick={navigateToSignup}>
+            회원가입
+          </Button>
+        </li>
+      </ul>
+    </header>
+  );
+}
