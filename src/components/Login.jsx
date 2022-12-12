e331239a7f7cdcb4fcf816e49ee81619282d15db
Melonisjy <dhy2457@naver.com>
2022-12-12 22:31:23 +0900

    "refactoring"

---
+++ src/components/Login.jsx
@@ -0,0 +1,125 @@
+import {
+  Checkbox,
+  TextField,
+  Button,
+  FormControlLabel,
+  Typography,
+  Container,
+  Box,
+  CssBaseline,
+  Grid,
+} from "@mui/material";
+import React, { useState } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link } from "react-router-dom";
+import sweetAlert from "sweetalert";
+
+const SERVER_URL = "http://localhost:3026/users/login";
+const theme = createTheme();
+
+async function loginUser(credentials) {
+  return fetch(SERVER_URL, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
+
+export default function LoginPage() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await loginUser({
+      email,
+      password,
+    });
+
+    if ("accessToken" in response) {
+      sweetAlert("Success", response.message, "success", {
+        buttons: false,
+        timer: 2000,
+      }).then(() => {
+        localStorage.setItem("accessToken", response["accessToken"]);
+        localStorage.setItem("user", JSON.stringifyf(response["user"]));
+        window.location.href = "/mypage";
+      });
+    } else {
+      sweetAlert("Failed", response.message, "error");
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="비밀번호 기억하기"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              로그인
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  비밀번호를 잊어버리셨나요?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/SignUp" variant="body2">
+                  회원가입
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
---
+++ src/components/Signup.jsx
@@ -0,0 +1,124 @@
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import swal from "sweetalert";
+
+const SERVER_URL = "http://localhost:3026/users/register";
+const theme = createTheme();
+
+export default function SignupPage() {
+  const navigate = useNavigate();
+  const navigateToMain = () => {
+    navigate("/");
+  };
+  const navigateToLogin = () => {
+    navigate("/login");
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const repassword = e.target.repassword.value;
+    let body = JSON.stringify({ email, password });
+    if (email === "") {
+      alert("이메일을 입력해주세요!");
+      return;
+    } else if (password === "") {
+      alert("비밀번호를 입력해주세요!");
+      return;
+    } else if (repassword === "") {
+      alert("비밀번호 확인을 입력해주세요!");
+      return;
+    } else if (password !== repassword) {
+      alert("비밀번호가 일치하지 않습니다!");
+      return;
+    } else axios.post(SERVER_URL, body);
+    console.log(body);
+    swal("회원가입에 성공하였습니다!", "로그인 페이지로 이동합니다.");
+    navigateToLogin();
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5" onClick={navigateToMain}>
+            Welcome to Healthub
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={onSubmitHandler}
+            sx={{ mt: 3 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  label="이메일"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="repassword"
+                  label="비밀번호 확인"
+                  type="password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              회원가입
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  계정이 이미 있다면 이 곳에서 로그인 하세요.
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
