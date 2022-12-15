dbe9b2dd9feb20301081cc7e4999d4bf6b69b3a0
Melonisjy <dhy2457@naver.com>
2022-12-15 17:16:42 +0900

    first commit

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,22 +1,13 @@
-// import React from "react";
-// import { Navigate } from "react-router-dom";
-
-// function PrivateRoute({ authenticated, component: Component }) {
-//   return authenticated ? (
-//     Component
-//   ) : (
-//     <Navigate to="/" {...alert("접근할 수 없는 페이지입니다.")} />
-//   );
-// }
-
-// export default PrivateRoute;
-
-import React from "react";
 import { Navigate } from "react-router-dom";
-import IsLogin from "./IsLogin";
+import swal from "sweetalert";
 
 const PrivateRoute = ({ children }) => {
-  return !IsLogin ? <Navigate to="/login" /> : children;
+  const isLogin = localStorage.getItem("accessToken");
+  return isLogin ? (
+    children
+  ) : (
+    <Navigate to="/login" {...swal("로그인이 필요합니다.")} />
+  );
 };
 
 export default PrivateRoute;
