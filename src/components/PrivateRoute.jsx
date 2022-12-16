112e0311bf65835af779b5218d247405f6b9b45d
Melonisjy <dhy2457@naver.com>
2022-12-16 20:17:59 +0900

    "jwt없을 시 로그인 화면으로 전환될 때의 alert 삭제"

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,9 @@
 import { Navigate } from "react-router-dom";
-import swal from "sweetalert";
+// import swal from "sweetalert";
 
 const PrivateRoute = ({ children }) => {
   const isLogin = localStorage.getItem("accessToken");
-  return isLogin ? (
-    children
-  ) : (
-    <Navigate to="/login" {...swal("로그인이 필요합니다.")} />
-  );
+  return isLogin ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
