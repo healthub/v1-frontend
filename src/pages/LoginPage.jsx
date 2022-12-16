f2e55ca0df78aa78a83e2f61d175e28857422deb
Melonisjy <dhy2457@naver.com>
2022-12-16 20:30:32 +0900

    fix: "loginHeader 추가"

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,10 @@
 import Login from "../components/Login";
+import LoginHeader from "../components/Header/LoginHeader";
 
 export default function LoginPage() {
   return (
     <>
+      <LoginHeader />
       <Login />
     </>
   );
