0a8a924677e677a56b5f2d3626d6e80805f4260e
Melonisjy <dhy2457@naver.com>
2022-12-19 20:25:07 +0900

    edit:id 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFoundPage from "./pages/NotFoundPage";
 import MyPage from "./pages/Mypage";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/ProfilePage";
+import Edit from "./pages/EditPage";
 
 export default function App() {
   return (
@@ -16,6 +17,14 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="/edit/:id"
+          element={
+            <PrivateRoute>
+              <Edit />
+            </PrivateRoute>
+          }
+        />
         <Route
           path="/profile"
           element={
